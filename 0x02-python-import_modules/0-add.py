618e64249854784c3ea148654bdb0c51fd4334d8
csewe56 <csewe56@gmail.com>
2023-11-30 05:14:11 -0800

    0x02. Python - import & modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+from add_0 import add
+
 a = 1
 b = 2
-add_0 = __import__('add_0')
-print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+result = add(a, b)
+print("{} + {} = {}".format(a, b, result))
 
+if __name__ == "__main__":
+    pass
