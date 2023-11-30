232cf624bbed3a22391ed2f7bc08b4f99dadb033
csewe56 <csewe56@gmail.com>
2023-11-30 05:28:11 -0800

    0x02. Python - import & modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-a = 10
-b = 5
-calculator_1 = __import__('calculator_1')
+from calculator_1 import add, sub, mul, div
 
-res_add = calculator_1.add(a, b)
-res_sub = calculator_1.sub(a, b)
-res_mul = calculator_1.mul(a, b)
-res_div = calculator_1.div(a, b)
+if __name__ == "__main__":
+    a = 10
+    b = 5
 
-print("{} + {} = {}".format(a, b, res_add))
-print("{} - {} = {}".format(a, b, res_sub))
-print("{} * {} = {}".format(a, b, res_mul))
-print("{} / {} = {}".format(a, b, res_div))
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {sub(a, b)}")
+    print(f"{a} * {b} = {mul(a, b)}")
+    print(f"{a} / {b} = {div(a, b)}")
 
