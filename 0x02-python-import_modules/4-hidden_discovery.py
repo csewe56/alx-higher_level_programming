6efa07ceef83a12628fc0f21c93633e1dee24862
csewe56 <csewe56@gmail.com>
2023-11-30 23:08:35 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,26 +1,10 @@
 #!/usr/bin/python3
-import dis
-import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./4-hidden_discovery.py <hidden_4.pyc>")
-        sys.exit(1)
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
 
-    filename = sys.argv[1]
-
-    with open(filename, 'rb') as file:
-        magic = file.read(4)
-        timestamp = file.read(4)
-        code = file.read()
-
-    bytecode = dis.Bytecode(code)
-    names = set()
-
-    for instruction in bytecode:
-        if 'LOAD_GLOBAL' in instruction.opname and not instruction.argrepr.startswith('__'):
-            names.add(instruction.argrepr)
-
-    for name in sorted(names):
-        print(name)
-
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
