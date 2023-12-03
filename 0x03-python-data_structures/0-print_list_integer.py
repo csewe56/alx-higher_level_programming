8d9772d265f13d45236517560490cadc5271673e
csewe56 <csewe56@gmail.com>
2023-12-03 13:18:54 -0800

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+
 def print_list_integer(my_list=[]):
-    x = my_list
-    for i in x:
-        print("{:d}".format(i))
+    """Print all integers of a list, one integer per line."""
+    for num in my_list:
+        print("{}".format(num))
+
+if __name__ == "__main__":
+    my_numbers = [1, 2, 3, 4, 5]
+    print_list_integer(my_numbers)
+
 
