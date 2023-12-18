ee578d20d327a048f7b3eff2dc66337284c33434
csewe56 <csewe56@gmail.com>
2023-12-18 13:13:09 -0800

    0x05. Python - Exceptions

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+#include <stdio.h>
+#include <Python.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+void print_python_list(PyObject *p)
+void print_python_bytes(PyObject *p)
+void print_python_float(PyObject *p
+
+#!/usr/bin/python3 -u
+
+import ctypes
+
+lib = ctypes.CDLL('./libPython.so')
+lib.print_python_list.argtypes = [ctypes.py_object]
+lib.print_python_bytes.argtypes = [ctypes.py_object]
+lib.print_python_float.argtypes = [ctypes.py_object]
+s = b"Hello"
+lib.print_python_bytes(s);
+
