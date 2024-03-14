b70086afb525132772f861975d4e5fa77d41705d
csewe56 <csewe56@gmail.com>
2024-03-14 13:30:09 -0700

    0x0D. SQL - Introduction

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,3 @@
--- Lists the number of records with the same score in the table second_table.
--- Records are ordered by descending count.
-SELECT `score`, COUNT(*) AS `number`
-FROM `second_table`
-GROUP BY `score`
-ORDER BY `number` DESC;
+-- lists all records of the table
+
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,13 @@
--- Creates and fills a table second_table with attributes id, name and score.
-CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "Peter", 10);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Robinson", 3);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Eric", 14);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "Allan", 8);
+-- creates a table second_table
+
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+
+INSERT INTO second_table (id, name , score) VALUES
+	(1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8);
