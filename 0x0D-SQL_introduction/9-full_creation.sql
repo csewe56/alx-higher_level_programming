ee0335ed2d4f11e753ad6cddbe1dfaee9dcd9341
csewe56 <csewe56@gmail.com>
2024-03-14 12:47:12 -0700

    0x0D. SQL - Introduction

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- Creates and fills a table second_table with attributes id, name and score.
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "Peter", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Robinson", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Eric", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "Allan", 8);
